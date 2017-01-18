93d98239d398fa6463cae3ee4a7ff2bd17e5d94c
artur-sak13 <artursak1981@gmail.com>
2017-01-17 22:42:30 -0600

    removed extraneous /doc directory

--- docs/js/fb.js
+++
@@ -1,7 +0,0 @@
-(function(d, s, id) {
-	var js, fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) return;
-	js = d.createElement(s); js.id = id;
-	js.src = "//connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v2.8&appId=1076723772443803";
-	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
